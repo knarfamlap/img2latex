0a1b0f3aed7d6894910c7c220ba966a3b2cdd587
Frank Palma <frankpalma12@gmail.com>
2020-09-16 22:42:52 -0400

    adds decoder

--- model/decoder.py
+++ model/decoder.py
@@ -2,12 +2,46 @@ import numpy as np
 import tensorflow as tf
 
 
-class Decoder(self):
-    def __init__(self):
-        super().__init__()
+class Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(Decoder, self), __init__()
+        self.units = units
 
-    def call(seq):
-        img_mean = tf.reduce_mean(seq, axis=1)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.lstm = tf.keras.layers.LSTM(self, units, return_sequences=True, return_state=True)
 
-        W1_e = tf.keras.layers.Dense(512)(seq)
-        W2_h = tf.keras.layers.Dense(512)()
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+        # TODO: Define Attention Cell
+        self.attention = # get attention cell
+
+    def call(self, x, features, hidden):
+        # defining attention as seperate model
+        context_vector, attention_weights = self.attention(features, hidden)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+        
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        
+        # passsing concatenated vector to LSTM
+        output, state = self.lstm(x)
+        
+        # shape == (batch_size, max_length, hidden_size)
+        x = self.fc1(output)
+
+        # x shape == (batch_size, max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
+
+        # output shape == (batch_size, * max_length, vocab)
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.units))
+
+
+

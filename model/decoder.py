4e1376359e1936d4346fa04974b78c5b69240c7f
Frank Palma <frankpalma12@gmail.com>
2020-09-26 19:23:06 -0400

    trains on small dataset

--- model/decoder.py
+++ model/decoder.py
@@ -5,27 +5,22 @@ from .attention import BahdanauAttention
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, embedding_dim, units, vocab_size, id_end):
+    def __init__(self, embedding_dim, units, vocab_size):
         super(Decoder, self). __init__()
         self.units = units
-        # comes from Vocab
-        self._id_end = id_end
 
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.lstm = tf.keras.layers.LSTMCell(self.units)
-
+        self.gru = tf.keras.layers.GRU(self.units,
+                                    return_sequences=True,
+                                    return_state=True,
+                                    recurrent_initializer='glorot_uniform')
         self.fc1 = tf.keras.layers.Dense(self.units)
         self.fc2 = tf.keras.layers.Dense(vocab_size)
 
         self.attention = BahdanauAttention(self.units)
 
-        self.start_token = tf.Variable(initial_value=embedding_initializer(embedding_dim, dtype=tf.float32), dtype=tf.float32, shape=[embedding_dim],)
-
-        self.decoder = tfa.seq2seq.BeamSearchDecoder(
-            self.lstm, beam_width=3, output_layer=self.fc1, )
-
     def call(self, x, features, hidden):
-        # defining attention as seperate model
+        # defining attention as a separate model
         context_vector, attention_weights = self.attention(features, hidden)
 
         # x shape after passing through embedding == (batch_size, 1, embedding_dim)
@@ -34,16 +29,16 @@ class Decoder(tf.keras.Model):
         # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
-        # passsing concatenated vector to LSTM
-        output, state = self.decoder(x, start_tokens=self.start_token)
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
 
         # shape == (batch_size, max_length, hidden_size)
-        # x = self.fc1(output)
+        x = self.fc1(output)
 
-        # x shape == (batch_size, max_length, hidden_size)
-        x = tf.reshape(output, (-1, x.shape[2]))
+        # x shape == (batch_size * max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
 
-        # output shape == (batch_size, * max_length, vocab)
+        # output shape == (batch_size * max_length, vocab)
         x = self.fc2(x)
 
         return x, state, attention_weights
@@ -51,9 +46,7 @@ class Decoder(tf.keras.Model):
     def reset_state(self, batch_size):
         return tf.zeros((batch_size, self.units))
 
-
 def embedding_initializer(shape, dtype):
     E = tf.random.uniform(shape, minval=-1.0, maxval=1.0, dtype=dtype)
     E = tf.nn.l2_normalize(E, -1)
     return E
-

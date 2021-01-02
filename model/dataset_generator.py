a32f0dddf34a53fed1fa3bd806d3134b1475b155
knarfamlap <frankpalma12@gmail.com>
2021-01-01 21:35:44 -0500

    update

---
+++ model/dataset_generator.py
@@ -0,0 +1,32 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from skimage import io
+
+
+class DatasetGenerator(Dataset):
+
+    def __init__(self, formulas_file, root_dir):
+        """
+        Args:
+            formulas_file (String): Path to the formulas file
+            root_dir (String): Directory with all the images in png format
+        """
+        self.formulas = open(formulas_file, 'r').read().split('\n')[:-1]
+        self.root_dir = root_dir
+
+    def __len__(self):
+        return len(self.formulas)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, "{}.png".format(idx))
+
+        image = io.imread(img_name)
+        formula = self.formulas[idx]
+
+        sample = {'image': image, 'formula': formula}
+
+        return sample
